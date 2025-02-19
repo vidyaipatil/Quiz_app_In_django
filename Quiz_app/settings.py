a708be3844e923a7a4aec92513fdd06455b54e31
vidya <vidyaipatil2405@gmail.com>
2025-02-19 19:32:15 +0530

    modify setting.py

--- Quiz_app/settings.py
+++ Quiz_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#po+m=-=#*1e^qvvhyux7nik=#+tc09uh6&nwvo4^kns1e@$5w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
